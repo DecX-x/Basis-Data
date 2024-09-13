8d2811b305b9ddb8008e4190f28c716f7439f4c4
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2024-09-13 12:17:57 +0700

    remove

--- Pertemuan 2/Latihan2.sql
+++
@@ -1,4 +0,0 @@
-# Mengupdate Data pada row Ke-2
-UPDATE employee
-SET id_emp = '234', name_emp= 'Dani', age=23
-WHERE row_id = 2;
--- Pertemuan 2/Latihan9.sql
+++
@@ -1,9 +0,0 @@
-# Memulai transaksi
-START TRANSACTION;
-
-# Melakukan operasi INSERT INTO
-INSERT INTO Employee (id, name_emp, age, gol)
-VALUES (112, 'Budi 2', 21, 'III');
-
-# Membatalkan operasi INSERT INTO
-ROLLBACK;
