126695ffe9c4f7ab6d8e60de04a6651512f4b7ae
Muhammad Ellbendl Satria <69744381+DecX-x@users.noreply.github.com>
2024-09-13 14:09:36 +0700

    Update Latihan8.sql

--- Pertemuan 2/Latihan8.sql
+++ Pertemuan 2/Latihan8.sql
@@ -3,18 +3,18 @@ CREATE TABLE Employeecopy AS
 SELECT * FROM Employee WHERE 1=0;
 
 # Mengisi data ke dalam tabel Employeecopy
-INSERT INTO Employeecopy (id, name_emp, age, gol)
+INSERT INTO Employeecopy (id_emp, name_emp, age, gol)
 VALUES
-(1, 'Budi B', 23, 'I'),
-(2, 'Dani', 22, 'II'),
-(3, 'Intan', 24, 'II'),
-(4, 'Budi A', 25, 'III'),
-(5, 'Buid w', 21, 'I');
+('1', 'Budi B', 23, 'I'),
+('2', 'Dani', 22, 'II'),
+('3', 'Intan', 24, 'II'),
+('4', 'Budi A', 25, 'III'),
+('5', 'Buid w', 21, 'I');
 
 # Menggunakan INSERT INTO ... SELECT ... 
-INSERT INTO Employee (id, name_emp, age, gol)
-SELECT id, name_emp, age, gol
-FROM Employee_copy
+INSERT INTO Employee (id_emp, name_emp, age, gol)
+SELECT id_emp, name_emp, age, gol
+FROM Employeecopy
 WHERE age > 21;
 
 
